ac6df98be458418837b88bf5951c293dea51f0eb
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-05 17:19:34 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     Rawg(apiKey: "ff98fccb8e2e4f249d657a82a2d4ac60");
@@ -19,6 +18,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff212129)),
         useMaterial3: true,
+        textTheme: TextTheme(
+          bodyText1: TextStyle(),
+          bodyText2: TextStyle(),
+        ).apply(
+          bodyColor: Colors.white, 
+          displayColor: Colors.white, 
+        ), 
       ),
       builder: EasyLoading.init(),
       home: const HomePage(),
