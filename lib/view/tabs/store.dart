2fe75130ecfcf2de8a8cf1092fdba855e84f4bb8
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-06 16:54:45 +0100

    Update store.dart

--- lib/view/tabs/store.dart
+++ lib/view/tabs/store.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class StoreTab extends StatefulWidget {
+  const StoreTab({super.key});
+
+  @override
+  State<TrophyPage> createState() => _TrophyPageState();
+}
+
+class _TrophyPageState extends State<TrophyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
