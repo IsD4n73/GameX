ee9005d07a60169bea65bfff7107c10fc2ef7055
IsD4n73 <IsD4n73@users.noreply.github.com>
2024-01-06 10:25:29 +0000

    deploy: 351189196be86261b461d58affc9b4d44a497eb4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "135d35320ebd578fc0dc43fd0a070c51",
-"index.html": "24f85c975bcf3f731788652e18da2277",
-"/": "24f85c975bcf3f731788652e18da2277",
+"index.html": "a6ad2072f6247b1b67bee1f7da3a12b7",
+"/": "a6ad2072f6247b1b67bee1f7da3a12b7",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/fonts/MaterialIcons-Regular.otf": "17820fab329932895eac704326416b85",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
