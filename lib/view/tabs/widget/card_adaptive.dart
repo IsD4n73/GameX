58a2958b788f30ccf09625231aacf0a760910eea
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-01-06 13:42:34 +0100

    Update card_adaptive.dart

--- lib/view/tabs/widget/card_adaptive.dart
+++ lib/view/tabs/widget/card_adaptive.dart
@@ -36,7 +36,7 @@ class CardAdaptive extends StatelessWidget {
     ),
   ),
 ).animate(onPlay: (controller) => controller.repeat())
- .shimmer(duration: 1200.ms, color: shimmer ? const Color(0xFF80DDFF) : Colors.transparent)
+ .shimmer(duration: 1200.ms, color: shimmer ? const Color(0xFF5156A9) : Colors.transparent)
  .animate() 
 );
   }
